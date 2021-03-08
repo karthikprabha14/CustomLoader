82f28060fba4bd1d71f1e4cc12f620215327992a
Karthik M <karthik.m1@photoninfotech.net>
2021-03-08 21:58:06 +0530

    Initial Commit

---
+++ CustomLoader/CustomLoader.h
@@ -0,0 +1,18 @@
+//
+//  CustomLoader.h
+//  CustomLoader
+//
+//  Created by Karthik M on 08/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomLoader.
+FOUNDATION_EXPORT double CustomLoaderVersionNumber;
+
+//! Project version string for CustomLoader.
+FOUNDATION_EXPORT const unsigned char CustomLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomLoader/PublicHeader.h>
+
+
