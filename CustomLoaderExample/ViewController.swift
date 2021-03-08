5c58b65ea0f1aaf59a156cfab212cacfb6c7eee7
Karthik M <karthik.m1@photoninfotech.net>
2021-03-08 22:20:41 +0530

    example target

---
+++ CustomLoaderExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomLoaderExample
+//
+//  Created by Karthik M on 08/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
